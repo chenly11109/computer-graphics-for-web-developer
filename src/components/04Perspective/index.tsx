d72b797d096d336da488104dace3f4cb0d7ba88b
陈凌亚 <chenlingya@tezign.com>
2023-09-07 15:05:12 +0800

    change to pnpm

--- src/components/04Perspective/index.tsx
+++ src/components/04Perspective/index.tsx
@@ -2,18 +2,13 @@ import { Link } from "react-router-dom"
 
 export default function Transformation2D(){
     return <section>
-        <Link className="font-bold underline" to="/webgpu/03transformation3D">Demo</Link>
+        <Link className="font-bold underline" to="/webgpu/04perspective">Demo</Link>
 
-        <h1 className="text-2xl">Transformation</h1>
-        <h2>Rotation</h2>
-
-        <br className="mt-5"/>
-        <h1 className="text-2xl">Clipping Plan</h1>
-        <h2>Ortho</h2>
+        <h1 className="text-2xl">frustum perspective</h1>
 
         <br className="mt-5"/>
-        <h1 className="text-2xl">WebGPU</h1>
-        <h2>depth</h2>
-        <h2>alpha and blend mode</h2>
+        <h1 className="text-2xl">Camera</h1>
+        <h2>up</h2>
+        <h2>lookAt</h2>
     </section>
 }
