59b98ad4345ddabe8b0abd43d056aea1795b0339
陈凌亚 <chenlingya@tezign.com>
2024-07-17 15:09:12 +0800

    fix: base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/computer-graphics-for-web-developer/notes/01basics"
+  base: "/computer-graphics-for-web-developer/"
 })
